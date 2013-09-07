fe116af30c275cb3a65c686b6fc1f76999d79b49
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-09-07 05:26:34 +0200

    add sulfur/schema/pattern to encapsulate an XSD pattern and its source

---
+++ src/sulfur/schema/pattern.js
@@ -0,0 +1,85 @@
+/* Copyright (c) 2013, Erik Wienhold
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License.
+ */
+
+/* global define */
+
+define([
+  'sulfur/object',
+  'sulfur/schema/regex'
+], function ($object, $regex) {
+
+  'use strict';
+
+  /**
+   * A pattern encapsulates a compiled regular expression and its source.
+   * Provides a simple interface to match a string against the pattern.
+   */
+
+  var $ = $object.clone({
+
+    /**
+     * Check if a string represents a valid XSD pattern.
+     *
+     * @param [string] s the string representation
+     *
+     * @return [boolean] whether the string representation is a valid pattern
+     */
+    isValidLiteral: function (s) {
+      try {
+        $regex.parse(s);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    }
+
+  });
+
+  $.augment({
+
+    /**
+     * Initialize the pattern by compiling the given source.
+     *
+     * @param [string] source the pattern's source
+     *
+     * @throw [Error] if sulfur/schema/regex.compile throws
+     */
+    initialize: function (source) {
+      var compiledPattern;
+      try {
+        compiledPattern = $regex.compile(source);
+      } catch (e) {
+        throw new Error('invalid pattern "' + source + '" (error: ' + e.message + ')');
+      }
+      this._source = source;
+      this._compiledPattern = compiledPattern;
+    },
+
+    /**
+     * Get the source from which the patterns was compiled.
+     *
+     * @return [string] the pattern's source
+     */
+    toLiteral: function () {
+      return this._source;
+    },
+
+    /**
+     * Check if a string matches the pattern.
+     *
+     * @param [string] s the string to test
+     *
+     * @return [boolean] whether `s` matches the pattern
+     */
+    test: function (s) {
+      return this._compiledPattern.test(s);
+    }
+
+  });
+
+  return $;
+
+});
---
+++ test/sulfur/schema/pattern.spec.js
@@ -0,0 +1,111 @@
+/* Copyright (c) 2013, Erik Wienhold
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License.
+ */
+
+/* global define */
+/* global afterEach, beforeEach, describe, it */
+
+define([
+  'shared',
+  'sulfur/schema/pattern',
+  'sulfur/schema/regex'
+], function ($shared, $pattern, $regex) {
+
+  'use strict';
+
+  var expect = $shared.expect;
+  var sinon = $shared.sinon;
+  var bind = $shared.bind;
+
+  describe.only('sulfur/schema/pattern', function () {
+
+    var sandbox;
+
+    beforeEach(function () {
+      sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+    });
+
+    describe('.isValidLiteral()', function () {
+
+      it("should call sulfur/schema/regex.parse() with the given string", function () {
+        var spy = sandbox.spy($regex, 'parse');
+        var s = '.';
+        $pattern.isValidLiteral(s);
+        expect(spy).to.be.calledWith(s);
+      });
+
+      it("should return true if sulfur/schema/regex.parse() does not throw", function () {
+        sandbox.stub($regex, 'parse');
+        expect($pattern.isValidLiteral()).to.be.true;
+      });
+
+      it("should return false if sulfur/schema/regex.parse() throws", function () {
+        sandbox.stub($regex, 'parse').throws();
+        expect($pattern.isValidLiteral()).to.be.false;
+      });
+
+    });
+
+    describe('#initialize()', function () {
+
+      it("should compile the source", function () {
+        var spy = sandbox.spy($regex, 'compile');
+        $pattern.create('.');
+        expect(spy).to.be.calledWith('.');
+      });
+
+      it("should throw when sulfur/schema/regex.compile throws", function () {
+        sandbox.stub($regex, 'compile').throws(new Error("invalid for testing purposes"));
+        expect(bind($pattern, 'create', '.'))
+          .to.throw('invalid pattern "." (error: invalid for testing purposes)');
+      });
+
+    });
+
+    describe('#toLiteral()', function () {
+
+      it("should return the pattern's source", function () {
+        var p = $pattern.create('[a-z]');
+        expect(p.toLiteral()).to.equal('[a-z]');
+      });
+
+    });
+
+    describe('#test()', function () {
+
+      it("should return true if the pattern matches the string", function () {
+        var p = $pattern.create('[a-z]');
+        expect(p.test('a')).to.be.true;
+      });
+
+      it("should return false if the pattern does not match the string", function () {
+        var p = $pattern.create('[a-z]');
+        expect(p.test('A')).to.be.false;
+      });
+
+      it("should call RegExp.prototype.test on the compiled pattern", function () {
+        var testSpy = sandbox.spy(RegExp.prototype, 'test');
+        var compileSpy = sandbox.spy($regex, 'compile');
+        var p = $pattern.create('.');
+        p.test();
+        expect(testSpy).to.be.calledOn(compileSpy.getCall(0).returnValue);
+      });
+
+      it("should pass the string to RegExp.prototype.test", function () {
+        var testSpy = sandbox.spy(RegExp.prototype, 'test');
+        var p = $pattern.create('.');
+        p.test('x');
+        expect(testSpy).to.be.calledWith('x');
+      });
+
+    });
+
+  });
+
+});
