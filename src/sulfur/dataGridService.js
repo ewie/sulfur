a06736311b81a9aa9fbeb3e7be5e11df4c6c8d9b
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-12-31 19:36:31 +0100

    add sulfur/dataGridService

---
+++ src/sulfur/dataGridService.js
@@ -0,0 +1,83 @@
+/* Copyright (c) 2013, Erik Wienhold
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License.
+ */
+
+/* global define */
+
+define(['sulfur/util/factory'], function (Factory) {
+
+  'use strict';
+
+  var NS_DGS = 'http://www.webcomposition.net/2008/02/dgs/';
+  var DGS_XML_DATA_SPACE_ENGINE = NS_DGS + 'DataSpaceEngines/XmlDataSpaceEngine';
+  var DGS_BINARY_DATA_SPACE_ENGINE = NS_DGS + 'DataSpaceEngines/BinaryDataSpaceEngine';
+
+  function createCollectionDefinition(title, dataSpaceEngine) {
+    // XXX
+    //   Generate a string rather than a Document object. The DataGridService
+    //   won't handle a document with unqualified attribute "type" used by
+    //   element "dataspaceengine", and the XML serializer won't generate a
+    //   prefixed attribute either.
+    return (
+      '<collection xmlns="http://www.w3.org/2007/app">' +
+       '<atom:title xmlns:atom="http://www.w3.org/2005/Atom">' + title + '</atom:title>' +
+       '<dgs:dataspaceengines xmlns:dgs="' + NS_DGS + '">' +
+        '<dgs:dataspaceengine dgs:type="' + dataSpaceEngine + '"/>' +
+       '</dgs:dataspaceengines>' +
+      '</collection>');
+  }
+
+  function concatPath(x, y) {
+    (x.charAt(x.length - 1) !== '/') && (x += '/');
+    x += y;
+    return x;
+  }
+
+  return Factory.derive({
+
+    initialize: function (endpoint) {
+      if (!endpoint) {
+        throw new Error("expecting a non-empty endpoint URL");
+      }
+      this._endpoint = endpoint;
+    },
+
+    get endpoint() { return this._endpoint },
+
+    get relationshipsEndpoint() { return concatPath(this.endpoint, 'meta/relationships') },
+
+    recordCollectionUrl: function (resource) {
+      return concatPath(this.endpoint, resource.name);
+    },
+
+    fileCollectionUrl: function (resource) {
+      return concatPath(this.endpoint, resource.name + '-files');
+    },
+
+    schemaUrl: function (resource) {
+      return concatPath(this.recordCollectionUrl(resource), 'meta/schema');
+    },
+
+    recordCollectionDefinition: function (resource) {
+      return createCollectionDefinition(resource.name, DGS_XML_DATA_SPACE_ENGINE);
+    },
+
+    fileCollectionDefinition: function (resource) {
+      return createCollectionDefinition(resource.name + '-files', DGS_BINARY_DATA_SPACE_ENGINE);
+    },
+
+    recordFileRelationDefinition: function (resource) {
+      return '@prefix dm:<' + NS_DGS + 'meta/>.\n' +
+        '<> dm:source <' + this.recordCollectionUrl(resource) + '>.\n' +
+        '<> dm:target <' + this.fileCollectionUrl(resource) + '>.\n' +
+        '<> dm:predicate <urn:example:hasFile>.\n' +
+        '<> dm:inverse-predicate <urn:example:belongsToResource>.\n' +
+        '<> dm:target-alias "files".\n' +
+        '<> dm:source-alias "resource".\n';
+    }
+
+  });
+
+});
---
+++ test/sulfur/dataGridService.spec.js
@@ -0,0 +1,146 @@
+/* Copyright (c) 2013, Erik Wienhold
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License.
+ */
+
+/* global define */
+/* global describe, it */
+
+define([
+  'shared',
+  'sulfur/dataGridService'
+], function (shared, DataGridService) {
+
+  'use strict';
+
+  var expect = shared.expect;
+  var bind = shared.bind;
+
+  describe('sulfur/dataGridService', function () {
+
+    describe('#initialize()', function () {
+
+      it("should reject an empty DataGridService URL", function () {
+        expect(bind(DataGridService, 'create', ''))
+          .to.throw("expecting a non-empty endpoint URL");
+      });
+
+    });
+
+    describe('#endpoint', function () {
+
+      it("should return the DataGridService endpoint URL", function () {
+        var url = 'http://example.org';
+        var dgs = DataGridService.create(url);
+        expect(dgs.endpoint).to.equal(url);
+      });
+
+    });
+
+    describe('#relationshipsEndpoint', function () {
+
+      it("should return the DataGridService relationships endpoint URL", function () {
+        var dgs = DataGridService.create('http://example.org');
+        expect(dgs.relationshipsEndpoint).to.equal(dgs.endpoint + '/meta/relationships');
+      });
+
+    });
+
+    describe('#schemaUrl()', function () {
+
+      it("should return the record collection's schema URL", function () {
+        var dgs = DataGridService.create('http://example.org');
+        var resource = { name: 'foo' };
+        expect(dgs.schemaUrl(resource)).to.equal(dgs.recordCollectionUrl(resource) + '/meta/schema');
+      });
+
+    });
+
+    describe('#recordCollectionUrl()', function () {
+
+      it("should return the URL", function () {
+        var dgs = DataGridService.create('http://example.org');
+        var resource = { name: 'xyz' };
+        expect(dgs.recordCollectionUrl(resource)).to.equal(dgs.endpoint + '/' + resource.name);
+      });
+
+      it("should handle an endpoint URL with trailing slash", function () {
+        var dgs = DataGridService.create('http://example.org/');
+        var resource = { name: 'abc' };
+        expect(dgs.recordCollectionUrl(resource)).to.equal(dgs.endpoint + resource.name);
+      });
+
+    });
+
+    describe('#fileCollectionUrl()', function () {
+
+      it("should return the URL", function () {
+        var dgs = DataGridService.create('http://example.org');
+        var resource = { name: 'xyz' };
+        expect(dgs.fileCollectionUrl(resource)).to.equal(dgs.endpoint + '/' + resource.name + '-files');
+      });
+
+      it("should handle an endpoint URL with trailing slash", function () {
+        var dgs = DataGridService.create('http://example.org/');
+        var resource = { name: 'abc' };
+        expect(dgs.fileCollectionUrl(resource)).to.equal(dgs.endpoint + resource.name + '-files');
+      });
+
+    });
+
+    describe('#recordCollectionDefinition()', function () {
+
+      it("should return a string representing an XML document with the record collection definition", function () {
+        var dgs = DataGridService.create('http://example.org');
+        var resource = { name: 'foo' };
+        var s = dgs.recordCollectionDefinition(resource);
+        expect(s).to.equal(
+          '<collection xmlns="http://www.w3.org/2007/app">' +
+           '<atom:title xmlns:atom="http://www.w3.org/2005/Atom">' + resource.name + '</atom:title>' +
+           '<dgs:dataspaceengines xmlns:dgs="http://www.webcomposition.net/2008/02/dgs/">' +
+            '<dgs:dataspaceengine dgs:type="http://www.webcomposition.net/2008/02/dgs/DataSpaceEngines/XmlDataSpaceEngine"/>' +
+           '</dgs:dataspaceengines>' +
+          '</collection>');
+      });
+
+    });
+
+    describe('#fileCollectionDefinition()', function () {
+
+      it("should return a string representing an XML document with the file collection definition", function () {
+        var dgs = DataGridService.create('http://example.org');
+        var resource = { name: 'foo' };
+        var s = dgs.fileCollectionDefinition(resource);
+        expect(s).to.equal(
+          '<collection xmlns="http://www.w3.org/2007/app">' +
+           '<atom:title xmlns:atom="http://www.w3.org/2005/Atom">' + resource.name + '-files</atom:title>' +
+           '<dgs:dataspaceengines xmlns:dgs="http://www.webcomposition.net/2008/02/dgs/">' +
+            '<dgs:dataspaceengine dgs:type="http://www.webcomposition.net/2008/02/dgs/DataSpaceEngines/BinaryDataSpaceEngine"/>' +
+           '</dgs:dataspaceengines>' +
+          '</collection>');
+      });
+
+    });
+
+    describe('#recordFileRelationDefinition()', function () {
+
+      it("should return a XML document with the resource definition", function () {
+        var dgs = DataGridService.create('http://example.org');
+        var resource = { name: 'foo' };
+        var d = dgs.recordFileRelationDefinition(resource);
+        expect(d).to.equal(
+          '@prefix dm:<http://www.webcomposition.net/2008/02/dgs/meta/>.\n' +
+          '<> dm:source <' + dgs.recordCollectionUrl(resource) + '>.\n' +
+          '<> dm:target <' + dgs.fileCollectionUrl(resource) + '>.\n' +
+          '<> dm:predicate <urn:example:hasFile>.\n' +
+          '<> dm:inverse-predicate <urn:example:belongsToResource>.\n' +
+          '<> dm:target-alias "files".\n' +
+          '<> dm:source-alias "resource".\n');
+      });
+
+    });
+
+  });
+
+});
